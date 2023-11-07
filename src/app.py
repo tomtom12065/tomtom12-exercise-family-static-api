79b0c8e6e92cfec0032ab534b3610e59a3a53767
tomtom12065 <100985000+tomtom12065@users.noreply.github.com>
2023-11-07 19:42:54 +0000

    final project

--- src/app.py
+++ src/app.py
@@ -21,6 +21,7 @@ def handle_invalid_usage(error):
     return jsonify(error.to_dict()), error.status_code
 
 # generate sitemap with all your endpoints
+
 @app.route('/')
 def sitemap():
     return generate_sitemap(app)
@@ -30,13 +31,27 @@ def handle_hello():
 
     # this is how you can use the Family datastructure by calling its methods
     members = jackson_family.get_all_members()
-    response_body = {
-        "hello": "world",
-        "family": members
-    }
+      
+    return jsonify(members),200
+
+@app.route("/member",methods=["POST"])
+def add_member():
+    member = request.json
+    jackson_family.add_member(member)
+    
+    return {}, 200
+
+@app.route("/member/<int:memberid>",methods=["DELETE"])
+def delete_member(memberid):
+    jackson_family.delete_member(memberid)
+    
+    return {"done":True},200
+@app.route("/member/<int:memberid>",methods=["GET"])
+def getmember(memberid):
+    member = jackson_family.get_member(memberid)
+    return jsonify(member)
 
 
-    return jsonify(response_body), 200
 
 # this only runs if `$ python src/app.py` is executed
 if __name__ == '__main__':
